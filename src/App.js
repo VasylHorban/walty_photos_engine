4907e13ae9018a614664f1ac71d9c94bc6069efa
Vasyl Horban <nagatolt427@gmail.com>
2021-03-15 19:44:30 +0200

    add search component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import Search from "./components/Search/Search";
+import { Container } from "react-bootstrap";
 function App() {
   return (
     <div className="App">
-      
+      <Container>
+        <Search />
+      </Container>
     </div>
   );
 }
