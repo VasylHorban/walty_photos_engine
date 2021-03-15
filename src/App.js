9d3004d9dc1d3c4fae906fd300e2897879d99483
Vasyl Horban <nagatolt427@gmail.com>
2021-03-15 20:40:50 +0200

    add ImageList component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Search from "./components/Search/Search";
 import { Container } from "react-bootstrap";
+import ImageList from "./components/ImageList/ImageList";
 function App() {
   return (
     <div className="App">
       <Container>
         <Search />
+        <ImageList/>
       </Container>
     </div>
   );
